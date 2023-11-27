071335f27df37933b9aee58b74f1604bc629b3bb
ArchLordRyu <017edkmm@gmail.com>
2023-11-27 12:00:38 +0900

    feat:viewにアバター画像を表示

--- src/lib/avamop_maker/components/MakerView/MakerView.tsx
+++ src/lib/avamop_maker/components/MakerView/MakerView.tsx
@@ -1,31 +1,63 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "jimp/browser/lib/jimp";
 import type { Jimp } from "jimp/browser/lib/jimp";
-import { MakerCanvasStatusGen } from "../functions/fetchData/MakerCanvasStatusGen";
+import { MakerCanvasSelectedPartsGen } from "../functions/fetchData/MakerCanvasSelectedPartsGen";
+import { MakerLayerCombineParts } from "../functions/imageProcess/MakerLayerCombineParts";
+import { MakerConvertBase64 } from "../functions/imageProcess/MakerConvertBase64";
 
 interface MakerViewProps {
   SelectedParts: SelectedParts;
   PartsObjectJimp: PartsObjectJimp;
   selectedFace: string;
+  scale: number;
 }
 
 const MakerView: React.FC<MakerViewProps> = ({
   SelectedParts,
   PartsObjectJimp,
   selectedFace,
+  scale,
 }) => {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
+  const [canvasImage, setCanvasImage] = useState<string | null>(null);
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const imageGen = async () => {
+      const selectedPartsForCanvas: SelectedPartsForCanvas =
+        MakerCanvasSelectedPartsGen(
+          SelectedParts,
+          PartsObjectJimp,
+          selectedFace
+        );
 
-  const SelectedPartsForCanvas = MakerCanvasStatusGen(
-    SelectedParts,
-    PartsObjectJimp,
-    selectedFace
-  );
+      const tmpCanvasImage: Jimp = await MakerLayerCombineParts(
+        selectedPartsForCanvas
+      );
 
+      setCanvasImage(await MakerConvertBase64(tmpCanvasImage));
+    };
+    imageGen();
+  }, [SelectedParts, selectedFace]);
+
+  useEffect(() => {
+    if (canvasImage && canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if (ctx) {
+        const img = new Image();
+        img.onload = () => {
+          ctx.clearRect(0, 0, canvas.width, canvas.height);
+          canvas.width = img.width * scale;
+          canvas.height = img.height * scale;
+          ctx.imageSmoothingEnabled = false;
+          ctx.drawImage(img, 0, 0, img.width * scale, img.height * scale);
+        };
+        img.src = canvasImage;
+      }
+    }
+  }, [canvasImage]);
   return (
     <>
-      <canvas></canvas> {/*アバター画像を表示する場所*/}
+      <canvas ref={canvasRef}></canvas> {/*アバター画像を表示する場所*/}
     </>
   );
 };
