dfb8a6e727088a0992bf2701dcc439c599875a69
SAWAtake <g022c6011@g.neec.ac.jp>
2023-12-18 14:29:41 +0900

    fix:エラーの除去

--- src/lib/avamop_maker/components/makerMenu/MakerFaceMenu.tsx
+++ src/lib/avamop_maker/components/makerMenu/MakerFaceMenu.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React, { useState, useEffect } from "react";
 import MakerFaceButton from "./MakerFaceButton";
 import styles from "../../module-css/makerMenu/MakerFaceMenu.module.css";
@@ -8,12 +8,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
 import 'swiper/css';
 
-=======
-import React, { useState } from "react";
-import MakerFaceButton from "./MakerFaceButton";
-import styles from "../../module-css/makerMenu/MakerFaceMenu.module.css"; // CSSファイルをインポート
-import { Swiper, SwiperSlide } from 'swiper/react';
->>>>>>> origin/Kadoya
 
 interface MakerFaceMenuProps {
   faceList: string[];
@@ -25,7 +19,7 @@ const MakerFaceMenu: React.FC<MakerFaceMenuProps> = ({
   faceList,
   changeFace,
 }) => {
-<<<<<<< HEAD
+
   const [showMenu, setShowMenu] = useState(false);
   const [buttonPosition, setButtonPosition] = useState({left: '4px', top: '0px'});
 
@@ -66,13 +60,13 @@ const MakerFaceMenu: React.FC<MakerFaceMenuProps> = ({
           alt="Show Faces" style={{width: '100px', height: '100px'}} />}
       </button>
     </div>
-=======
-  const [showSwiper, setShowSwiper] = useState(false);
-  return (
-    <>
+  )
+const [showSwiper, setShowSwiper] = useState(false);
+return (
+  <>
     <button className={styles["face-show-button"]} onClick={() => setShowSwiper(!showSwiper)}>
       {showSwiper ? <img className={styles["swiper-color-image"]} src="../../../../../examples/assets/provisionals/provisionalclose.png" alt="Hide Face" />
-          : <img className={styles["swiper-color-image"]} src="../../../../../examples/assets/provisionals/provisionalopen.png" alt="Show Face" />}
+        : <img className={styles["swiper-color-image"]} src="../../../../../examples/assets/provisionals/provisionalopen.png" alt="Show Face" />}
     </button>
     {showSwiper && (
       <Swiper
@@ -82,24 +76,24 @@ const MakerFaceMenu: React.FC<MakerFaceMenuProps> = ({
         grabCursor={true}
         spaceBetween={10}
       >
-    <ul className={styles['face-menu']}>
-      {faceList.map((face) => (
-        <SwiperSlide key={face} style={{ width: '180px' }}>
-        <MakerFaceButton
-          key={face}
-          face={face}
-          // 表情のサムネイルを用意する予定
-          // faceImages= {faceImages[face]}
-          onClick={() => changeFace(face)}
-        />
-        </SwiperSlide>
-      ))}
-    </ul>
-  </Swiper>
-  )}
+        <ul className={styles['face-menu']}>
+          {faceList.map((face) => (
+            <SwiperSlide key={face} style={{ width: '180px' }}>
+              <MakerFaceButton
+                key={face}
+                face={face}
+                // 表情のサムネイルを用意する予定
+                // faceImages= {faceImages[face]}
+                onClick={() => changeFace(face)}
+              />
+            </SwiperSlide>
+          ))}
+        </ul>
+      </Swiper>
+    )}
   </>
->>>>>>> origin/Kadoya
-  );
+);
+
 };
 
 export default React.memo(MakerFaceMenu);
