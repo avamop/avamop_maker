e5c92d5567bb6278f49cd1cc5304ee90178fbb40
ArchLordRyu <017edkmm@gmail.com>
2024-02-18 20:51:09 +0900

    fix:サーバーサイドで実行できるように、画像処理関数の型を変えたものを移植

--- types/components/functions/fetchData/MakerGenerateAvaterImage.d.ts
+++ types/components/functions/fetchData/MakerGenerateAvaterImage.d.ts
@@ -1,4 +1,35 @@
-import "jimp/browser/lib/jimp";
+import Jimp from "jimp";
+interface PartsObjectJimp {
+    [category: "body" | string]: {
+        partCount: number;
+        partChain: string;
+        ignoreTrigger: null | string[];
+        partList: CategoryJimp;
+    };
+}
+interface CategoryJimp {
+    [partSplit: "body" | string]: {
+        colorGroup: null | string;
+        partOrder: number;
+        items: ItemsJimp;
+    };
+}
+interface ItemsJimp {
+    [item: string]: {
+        bodyType: string[];
+        enableColor: boolean;
+        faces: FacesJimp;
+    };
+}
+interface FacesJimp {
+    [face: "clear" | string]: {
+        jimpData: Jimp;
+    };
+}
 declare const MakerGenerateAvaterImage: (partsPath: string, nullImagePath: string, partsObject: PartsObjectSplit, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<string>;
 export default MakerGenerateAvaterImage;
+export declare const MakerConvertPartsJimp: (partsObject: PartsObjectSplit, partsPath: string, nullImage: Jimp, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<PartsObjectJimp>;
+export declare const MakerPartsColoring: (image: Jimp, partSplit: string, colorGroup: string, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<Jimp>;
+export declare const MakerLayerCombineParts: (selectedPartsForCanvas: SelectedPartsForCanvas) => Promise<Jimp[]>;
+export declare const MakerCanvasSelectedPartsGen: (selectedParts: SelectedParts, partsObjectJimp: PartsObjectJimp, nullImage: Jimp) => SelectedPartsForCanvas;
 //# sourceMappingURL=MakerGenerateAvaterImage.d.ts.map
