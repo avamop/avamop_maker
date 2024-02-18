7dbe3603ee57cbac46922354080420e97b06ddfd
ArchLordRyu <017edkmm@gmail.com>
2024-02-18 21:04:34 +0900

    fix:Jimpの型情報を固有のものに

--- types/components/functions/fetchData/MakerGenerateAvaterImage.d.ts
+++ types/components/functions/fetchData/MakerGenerateAvaterImage.d.ts
@@ -1,4 +1,5 @@
 import Jimp from "jimp";
+type JimpServer = Jimp;
 interface PartsObjectJimpServer {
     [category: "body" | string]: {
         partCount: number;
@@ -23,13 +24,13 @@ interface ItemsJimpServer {
 }
 interface FacesJimpServer {
     [face: "clear" | string]: {
-        jimpData: Jimp;
+        jimpData: JimpServer;
     };
 }
 declare const MakerGenerateAvaterImage: (partsPath: string, nullImagePath: string, partsObject: PartsObjectSplit, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<string>;
 export default MakerGenerateAvaterImage;
-export declare const MakerConvertPartsJimp: (partsObject: PartsObjectSplit, partsPath: string, nullImage: Jimp, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<PartsObjectJimpServer>;
-export declare const MakerPartsColoring: (image: Jimp, partSplit: string, colorGroup: string, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<Jimp>;
+export declare const MakerConvertPartsJimp: (partsObject: PartsObjectSplit, partsPath: string, nullImage: JimpServer, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<PartsObjectJimpServer>;
+export declare const MakerPartsColoring: (image: JimpServer, partSplit: string, colorGroup: string, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<Jimp>;
 export declare const MakerLayerCombineParts: (selectedPartsForCanvas: SelectedPartsForCanvas) => Promise<Jimp[]>;
-export declare const MakerCanvasSelectedPartsGen: (selectedParts: SelectedParts, partsObjectJimp: PartsObjectJimpServer, nullImage: Jimp) => SelectedPartsForCanvas;
+export declare const MakerCanvasSelectedPartsGen: (selectedParts: SelectedParts, partsObjectJimp: PartsObjectJimpServer, nullImage: JimpServer) => SelectedPartsForCanvas;
 //# sourceMappingURL=MakerGenerateAvaterImage.d.ts.map
