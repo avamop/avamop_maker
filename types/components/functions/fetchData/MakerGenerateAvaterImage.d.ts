e0ffc6915a22c93fdd934f4ac27e2843e9f3a5b4
ArchLordRyu <017edkmm@gmail.com>
2024-02-18 22:42:45 +0900

    fix:型情報と関数を差し戻す

--- types/components/functions/fetchData/MakerGenerateAvaterImage.d.ts
+++ types/components/functions/fetchData/MakerGenerateAvaterImage.d.ts
@@ -1,36 +1,3 @@
-import Jimp from "jimp";
-import { JimpServer } from "../../../types/jimpServer";
-interface PartsObjectJimpServer {
-    [category: "body" | string]: {
-        partCount: number;
-        partChain: string;
-        ignoreTrigger: null | string[];
-        partList: CategoryJimpServer;
-    };
-}
-interface CategoryJimpServer {
-    [partSplit: "body" | string]: {
-        colorGroup: null | string;
-        partOrder: number;
-        items: ItemsJimpServer;
-    };
-}
-interface ItemsJimpServer {
-    [item: string]: {
-        bodyType: string[];
-        enableColor: boolean;
-        faces: FacesJimpServer;
-    };
-}
-interface FacesJimpServer {
-    [face: "clear" | string]: {
-        jimpData: JimpServer;
-    };
-}
 declare const MakerGenerateAvaterImage: (partsPath: string, nullImagePath: string, partsObject: PartsObjectSplit, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<string>;
 export default MakerGenerateAvaterImage;
-export declare const MakerConvertPartsJimp: (partsObject: PartsObjectSplit, partsPath: string, nullImage: JimpServer, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<PartsObjectJimpServer>;
-export declare const MakerPartsColoring: (image: JimpServer, partSplit: string, colorGroup: string, selectedParts: SelectedParts, colorsObject: ColorsObject) => Promise<JimpServer>;
-export declare const MakerLayerCombineParts: (selectedPartsForCanvas: SelectedPartsForCanvas) => Promise<Jimp[]>;
-export declare const MakerCanvasSelectedPartsGen: (selectedParts: SelectedParts, partsObjectJimp: PartsObjectJimpServer, nullImage: JimpServer) => SelectedPartsForCanvas;
 //# sourceMappingURL=MakerGenerateAvaterImage.d.ts.map
